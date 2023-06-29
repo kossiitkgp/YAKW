cade5b109c72f6f7a1fc96bc27f3607fe32188dc
Rajiv Harlalka <68731551+rajivharlalka@users.noreply.github.com>
2023-06-30 02:48:19 +0530

    add hero page
    Signed-off-by: Rajiv Harlalka <68731551+rajivharlalka@users.noreply.github.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,7 +20,7 @@ module.exports = {
       },
       rules: {
         // override/add rules settings here, such as:
-        // "astro/no-set-html-directive": "error"
+        "astro/no-set-html-directive": "warn"
       },
     },
     // ...
