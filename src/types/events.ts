94c1879b405e5a049ac80a979a7d517dbb7eafd3
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-09-26 14:14:38 +0530

    feat: updated events data

--- src/types/events.ts
+++ src/types/events.ts
@@ -1,14 +1,14 @@
 export interface IEventDate {
-	day: number;
-	month: number;
-	year: number;
+  day: number;
+  month: number;
+  year: number;
 }
 export interface IEvent {
-	title: string;
-	description: string;
-	date: IEventDate;
-	time: string;
-	location: string;
-	poster_img: string;
-	link: string;
+  title: string;
+  description: string;
+  date: IEventDate;
+  time: string;
+  location: string;
+  poster_img: string;
+  id: string;
 }
